14b54d6ba4b5fa15f93bd7fb31353f3330be67bb
hzejli <96780482+hzejli@users.noreply.github.com>
2024-06-02 22:13:43 +0200

    Add files via upload

--- 3D_cloud_buffer.js
+++ 3D_cloud_buffer.js
@@ -63,16 +63,20 @@ function startTimer() {
 
 function stopTimer() {
     clearInterval(timerInterval);
-    document.getElementById('timer').innerText = `Time Elapsed: 0s`;
+    document.getElementById('timer').innerText = `Temps Écoulé: 0s`;
 }
 
 function updateTimer() {
     const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-    document.getElementById('timer').innerText = `Time Elapsed: ${elapsedTime}s`;
+    document.getElementById('timer').innerText = `Temps Écoulé: ${elapsedTime}s`;
 }
-
 // HZEJLI - 01/06/2024 - END
 
+// HZEJLI - 01/06/2024 - BEGIN
+let lapCounter = 0;
+let distanceTraveled = 0;
+const galaxyRadius = 250; // Average radius of the galaxy
+// HZEJLI - 01/06/2024 - END
 
 
 
@@ -1454,6 +1458,24 @@ function animateLp() {
     cloud.updateCloud();
     controls.update();
     plotGalacticRotationCurve(stars);
+
+    // HZEJLI - 02/06/2024 - BEGIN
+    // Calculer la distance parcourue par une étoile
+    const star = stars[0];
+    const distance = Math.sqrt(star.velX * star.velX + star.velY * star.velY + star.velZ * star.velZ) * deltaTime;
+
+    // MAJ la distance totale parcourue
+    distanceTraveled += distance;
+
+    // Vérifier si un tour complet est réalisé
+    if (distanceTraveled >= 2 * Math.PI * galaxyRadius) {
+        lapCounter++;
+        distanceTraveled = 0; // Réinitialiser la distance parcourue
+    }
+    // MAJ l'affichage du compteur de tours
+    document.getElementById('lapCounter').innerText = `Nombre de Tours de la Galaxie: ${lapCounter}`;
+    // HZEJLI - 02/06/2024 - END
+
     renderer.render(scene, camera);
     
 }
